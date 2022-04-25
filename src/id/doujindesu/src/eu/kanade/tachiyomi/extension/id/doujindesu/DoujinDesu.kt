99f45cac1231a7895a0656e919f0b426c04c2cca
zxc743 <87969587+zxc743@users.noreply.github.com>
2022-04-26 05:41:27 +0700

    Update DoujinDesu.kt

--- src/id/doujindesu/src/eu/kanade/tachiyomi/extension/id/doujindesu/DoujinDesu.kt
+++ src/id/doujindesu/src/eu/kanade/tachiyomi/extension/id/doujindesu/DoujinDesu.kt
@@ -18,7 +18,7 @@ import java.util.Locale
 class DoujinDesu : ParsedHttpSource() {
     // Information : DoujinDesu use EastManga WordPress Theme
     override val name = "Doujindesu"
-    override val baseUrl = "https://doujindesu.xxx"
+    override val baseUrl = "https://dj.zxc743.xyz"
     override val lang = "id"
     override val supportsLatest = true
     override val client: OkHttpClient = network.cloudflareClient
